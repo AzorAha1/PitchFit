e66bd3d2a2274ae267e2b8c2d8ef0de1d451e151
faiz <wizfaiz@icloud.com>
2024-06-23 07:07:38 -0700

    sign in and sign up pages initialized

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,14 @@ app = Flask(__name__, template_folder='../frontend/templates/', static_folder='.
 def landingpage():
     """This is the landing page"""
     return render_template('index.html', title='Home')
-
+@app.route('/signin')
+def sigin():
+    """this is the sign in page"""
+    return render_template('signin.html', title='Sign In')
+@app.route('/signup')
+def signup():
+    """this is the sign up page"""
+    return render_template('signup.html', title='Sign Up')
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
 
