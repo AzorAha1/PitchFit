04013fe08281ae292597a5f755e915b81774872c
faiz <wizfaiz@icloud.com>
2024-06-24 15:38:06 -0700

    now signing in is possible

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,22 @@ mongo = PyMongo(app)
 def landingpage():
     """This is the landing page"""
     return render_template('index.html', title='Home')
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
     """this is the sign in page"""
-    return render_template('signin.html', title='Sign In')
+    error_message = None
+    if request.method == 'POST':
+        print("Form data received")
+        username = request.form['username']
+        password = request.form['password']
+        user = mongo.db.users.find_one({'username': username})
+        if user:
+            rehashed_password = bcrypt.checkpw(password.encode('utf-8'), user['password'])
+            if rehashed_password:
+                print(f'User {username} has been authenticated')
+                return redirect('/dashboard')
+        error_message = 'Invalid Credentials'
+    return render_template('signin.html', title='Sign In', error=error_message)
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     """this is the sign up page"""
@@ -43,6 +55,10 @@ def signup():
         print(f'User {username} has been created')
         return redirect('/signin')
     return render_template('signup.html', title='Sign Up')
+@app.route('/dashboard')
+def dashboard():
+    """this is the dashboard page"""
+    return render_template('dashboard.html', title='Dashboard')
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
 
