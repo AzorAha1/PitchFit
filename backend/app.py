e506645e2de0dec2824835bdb714326a56c86e8e
faiz <wizfaiz@icloud.com>
2024-06-28 16:49:27 -0700

    showing bmr and tdee for male or female

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, request, redirect, session, url_for
+from bson import ObjectId
+from flask import Flask, flash, render_template, request, redirect, session, url_for
 import bcrypt
 from flask_pymongo import PyMongo
 
@@ -7,10 +8,12 @@ app = Flask(__name__, template_folder='../frontend/templates/', static_folder='.
 app.secret_key = 'youwillneverfindoutwhatthisis'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/pitchfitdb'
 mongo = PyMongo(app)
+
 @app.route('/')
 def landingpage():
     """This is the landing page"""
     return render_template('index.html', title='Home')
+
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     """this is the sign in page"""
@@ -24,9 +27,11 @@ def signin():
             rehashed_password = bcrypt.checkpw(password.encode('utf-8'), user['password'])
             if rehashed_password:
                 print(f'User {username} has been authenticated')
+                session['user'] = user
                 return redirect('/dashboard')
         error_message = 'Invalid Credentials'
     return render_template('signin.html', title='Sign In', error=error_message)
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     """this is the sign up page"""
@@ -37,11 +42,11 @@ def signup():
         session['user']['email'] = request.form['email']
         session['user']['username'] = request.form['username']
         session['user']['password'] = request.form['password']
-        session['user']['name'] = request.form['preferred_name']
-        session['user']['weight'] = request.form['weight']
-        session['user']['height'] = request.form['height']
-        session['user']['gender'] = request.form['gender']
-        session['user']['age'] = request.form['age']
+        # session['user']['name'] = request.form['preferred_name']
+        # session['user']['weight'] = request.form['weight']
+        # session['user']['height'] = request.form['height']
+        # session['user']['gender'] = request.form['gender']
+        # session['user']['age'] = request.form['age']
         existing_user = mongo.db.users.find_one({'$or': [{'username': session['user']['username']}, {'email': session['user']['email']}]})
         if existing_user:
             print("User already exists")  # Debugging line
@@ -49,6 +54,8 @@ def signup():
 
         # using bcrpyt to hash password
         session['user']['password'] = bcrypt.hashpw(session['user']['password'].encode('utf-8'), bcrypt.gensalt())
+        result = mongo.db.users.insert_one(session['user'])
+        session['user']['_id'] = str(result.inserted_id) 
         return redirect(url_for('getname'))  
     return render_template('signup.html', title='Sign Up')
 
@@ -58,40 +65,47 @@ def getname():
     if request.method == 'POST':
         print(f"{request.form['preferred_name']}")
         session['user']['name'] = request.form['preferred_name']
-        
+        mongo.db.users.update_one({'username': session['user']['username']}, {'$set': {'name': session['user']['name']}})
         return redirect(url_for('getage'))
     return render_template('setup1.html', title='Setup Step 1')
+
 @app.route('/signup/step2', methods=['GET', 'POST'])
 def getage():
     """step 2 of setup"""
     if request.method == 'POST':
         print(f"{request.form['age']}")
         session['user']['age'] = request.form['age']
+        mongo.db.users.update_one({'username': session['user']['username']}, {'$set': {'age': session['user']['age']}})
         return redirect(url_for('getweight'))
     return render_template('setup2.html', title='Setup Step 2')
+
 @app.route('/signup/step3', methods=['GET', 'POST'])
 def getweight():
     """step 3 of setup"""
     if request.method == 'POST':
         print(f"{request.form['weight']}")
         session['user']['weight'] = request.form['weight']
+        mongo.db.users.update_one({'username': session['user']['username']}, {'$set': {'weight': session['user']['weight']}})
         return redirect(url_for('getheight'))
     return render_template('setup3.html', title='Setup Step 3')
+
 @app.route('/signup/step4', methods=['GET', 'POST'])
 def getheight():
     """step 4 of setup"""
     if request.method == 'POST':
         print(f"{request.form['height']}")
         session['user']['height'] = request.form['height']
-
+        mongo.db.users.update_one({'username': session['user']['username']}, {'$set': {'height': session['user']['height']}})
         return redirect(url_for('getgender'))
     return render_template('setup4.html', title='Setup Step 4')
+
 @app.route('/signup/step5', methods=['GET', 'POST'])
 def getgender():
     """get the gender of user"""
     if request.method == 'POST':
         session['user']['gender'] = request.form['gender']
         print(f"{request.form['gender']}")
+        mongo.db.users.update_one({'username': session['user']['username']}, {'$set': {'gender': session['user']['gender']}})
         return redirect(url_for('goals'))
     return render_template('getgender.html', title='Gender')
 
@@ -100,28 +114,71 @@ def weight_loss():
     """page for weight loss"""
     if request.method == 'POST':
         if request.form['weight_loss_complete'] == 'true':
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('deficit'))
     return render_template('weight_loss.html', title='Weight Loss')
+
 @app.route('/signup/goals', methods=['GET', 'POST'])
 def goals():
     """setting goals"""
     if request.method == 'POST':
         if 'selected_goals' in request.form:
             session['user']['goals'] = request.form['selected_goals']
-            mongo.db.users.insert_one(session['user'])
+            # mongo.db.users.insert_one(session['user'])
+            
             print(f'The Goals are: {session["user"]["goals"]}')
             goals = session['user']['goals']
-           
+            mongo.db.users.update_one({'username': session['user']['username']}, {'$set': {'goals': session['user']['goals']}})
             if 'Weight Loss' in goals:
-                weight_loss_redirect = True
-            else:
-                weight_loss_redirect = False
-            if weight_loss_redirect:
-                return redirect(url_for('weight_loss'))
+                return redirect(url_for('deficit'))
             return redirect(url_for('dashboard'))
         else:
             print("Error: 'selected_goals' key not found in form data.")
+    print(f'The User is: {session["user"]}')
     return render_template('goals.html', title='Goals')
+
+@app.route('/signup/deficit', methods=['GET', 'POST'])
+def deficit():
+    """This is the step to calculate calorie deficit"""
+    if request.method == 'POST':
+        # Check if 'calories_deficit' is in the form data
+        if 'calories_deficit' in request.form:
+            if request.form['calories_deficit'] == 'true':
+                session['user']['calories_deficit'] = True
+                return redirect(url_for('dashboard'))
+            else:
+                session['user']['calories_deficit'] = False
+        else:
+            # Handle the case where 'calories_deficit' is not in the form data
+            session['user']['calories_deficit'] = False  
+
+    
+    user = mongo.db.users.find_one({'_id': ObjectId(session['user']['_id'])})
+    weight = float(user['weight'])
+    height = float(user['height'])
+    age = int(user['age'])
+    gender = user['gender']
+    
+    if gender == 'male':
+        bmr = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age)
+    else:
+        bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age)
+    
+    tdee = bmr * 1.2  # Assuming a sedentary lifestyle by default
+    session['user']['bmr'] = bmr
+    session['user']['tdee'] = tdee
+
+    # Update the user document in MongoDB
+    mongo.db.users.update_one(
+        {'username': session['user']['username']},
+        {'$set': {'bmr': bmr, 'tdee': tdee}}
+    )
+    
+    print(f'BMR: {bmr}')
+    print(f'TDEE: {tdee}')
+    
+    return render_template('caloriesdeficit.html', title='Calorie Deficit', bmr=bmr, tdee=tdee)
+
+
 @app.route('/dashboard')
 def dashboard():
     """this is the dashboard page"""
@@ -133,12 +190,13 @@ def debug_users():
     users = mongo.db.users.find()
     users_list = list(users)
     return render_template('debug_users.html', users=users_list)
+
 @app.route('/signout')
 def signout():
     """this is the signout page"""
     session.pop('user', None)
     print('User has been signed out')
     return redirect(url_for('signin'))
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
